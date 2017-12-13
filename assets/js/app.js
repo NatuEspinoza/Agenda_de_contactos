e9d506ca78de3ef0fb61170e1cbb4b85476fe217
NatuEspinoza <natalia.utemdcv@gmail.com>
2017-12-12 21:23:24 -0300

    creando nueva rama

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
   $('.modal').modal();
+  $('.btn').click(function(){
+    var name = $('#icon_prefix').val();
+    var phone = $('#icon_telephone').val();
+    $('#contact').append(name + phone);
+    
+  })
 });
